19641b47c98be65af142719113118150092b4a5c
tonbach18598 <51219305+tonbach18598@users.noreply.github.com>
2019-09-20 17:55:48 +0700

    mvp demo

--- app/src/main/java/com/example/pcdashboard/Fragment/ForgotFragment.java
+++ app/src/main/java/com/example/pcdashboard/Fragment/ForgotFragment.java
@@ -2,20 +2,28 @@ package com.example.pcdashboard.Fragment;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
+import com.example.pcdashboard.Manager.ScreenManager;
 import com.example.pcdashboard.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ForgotFragment extends Fragment {
-
+public class ForgotFragment extends Fragment implements View.OnClickListener {
+    private ScreenManager screenManager;
+    private EditText etAccount;
+    private EditText etEmail;
+    private Button btnGetPassword;
+    private TextView tvBack;
 
     public ForgotFragment() {
         // Required empty public constructor
@@ -26,7 +34,31 @@ public class ForgotFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot, container, false);
+        View view = inflater.inflate(R.layout.fragment_forgot, container, false);
+        initialize(view);
+        return view;
     }
 
+    private void initialize(View view) {
+        screenManager = ScreenManager.getInstance();
+        etAccount = view.findViewById(R.id.et_account_forgot);
+        etEmail = view.findViewById(R.id.et_email_forgot);
+        btnGetPassword = view.findViewById(R.id.btn_get_forgot);
+        tvBack = view.findViewById(R.id.tv_back_forgot);
+        btnGetPassword.setOnClickListener(this);
+        tvBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_get_forgot:
+                Toast.makeText(getContext(), "Get Password", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_back_forgot:
+                Toast.makeText(getContext(), "Back", Toast.LENGTH_SHORT).show();
+                screenManager.openLoginScreen(1);
+                break;
+        }
+    }
 }
--- app/src/main/java/com/example/pcdashboard/Fragment/LoginFragment.java
+++ app/src/main/java/com/example/pcdashboard/Fragment/LoginFragment.java
@@ -2,9 +2,6 @@ package com.example.pcdashboard.Fragment;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.pcdashboard.Manager.IScreenManager;
+import androidx.fragment.app.Fragment;
+
 import com.example.pcdashboard.Manager.ScreenManager;
 import com.example.pcdashboard.R;
 import com.example.pcdashboard.View.ILoginView;
@@ -21,40 +19,39 @@ import com.example.pcdashboard.View.ILoginView;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment implements ILoginView,View.OnClickListener {
-private EditText etAccount;
-private EditText etPassword;
-private Button btnLogin;
-private TextView tvForgot;
-private ScreenManager screenManager;
+public class LoginFragment extends Fragment implements ILoginView, View.OnClickListener {
+    private ScreenManager screenManager;
+    private EditText etAccount;
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvForgot;
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
         initialize(view);
         return view;
     }
 
-    private void initialize(View view){
-        screenManager=ScreenManager.getInstance();
-        etAccount=view.findViewById(R.id.et_account_login);
-        etPassword=view.findViewById(R.id.et_password_login);
-        btnLogin=view.findViewById(R.id.btn_login_login);
-        tvForgot=view.findViewById(R.id.tv_forgot_login);
+    private void initialize(View view) {
+        screenManager = ScreenManager.getInstance();
+        etAccount = view.findViewById(R.id.et_account_login);
+        etPassword = view.findViewById(R.id.et_password_login);
+        btnLogin = view.findViewById(R.id.btn_login_login);
+        tvForgot = view.findViewById(R.id.tv_forgot_login);
         btnLogin.setOnClickListener(this);
         tvForgot.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_login_login:
                 Toast.makeText(getContext(), "Login", Toast.LENGTH_SHORT).show();
                 break;
