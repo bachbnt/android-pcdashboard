c10087bee11b9f5620f99708d2b9b3577b0becd5
tonbach18598 <51219305+tonbach18598@users.noreply.github.com>
2019-09-24 12:41:57 +0700

    post UI

---
+++ app/src/main/java/com/example/pcdashboard/Adapter/ClassroomAdapter.java
@@ -0,0 +1,4 @@
+package com.example.pcdashboard.Adapter;
+
+public class ClassroomAdapter {
+}
---
+++ app/src/main/java/com/example/pcdashboard/Model/Classroom.java
@@ -0,0 +1,67 @@
+package com.example.pcdashboard.Model;
+
+public class Classroom {
+    private String id;
+    private String classId;
+    private String avatar;
+    private String name;
+    private String time;
+    private String content;
+
+    public Classroom(String id, String classId, String avatar, String name, String time, String content) {
+        this.id = id;
+        this.classId = classId;
+        this.avatar = avatar;
+        this.name = name;
+        this.time = time;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
