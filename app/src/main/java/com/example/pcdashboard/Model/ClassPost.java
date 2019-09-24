e035bb758041218842adecdbcb0beec8cfacd068
tonbach18598 <51219305+tonbach18598@users.noreply.github.com>
2019-09-24 16:25:26 +0700

    api

--- app/src/main/java/com/example/pcdashboard/Model/ClassPost.java
+++ app/src/main/java/com/example/pcdashboard/Model/ClassPost.java
@@ -2,16 +2,14 @@ package com.example.pcdashboard.Model;
 
 public class ClassPost {
     private String id;
-    private String classId;
     private String avatar;
     private String name;
     private String time;
     private String content;
     private String image;
 
-    public ClassPost(String id, String classId, String avatar, String name, String time, String content, String image) {
+    public ClassPost(String id, String avatar, String name, String time, String content, String image) {
         this.id = id;
-        this.classId = classId;
         this.avatar = avatar;
         this.name = name;
         this.time = time;
@@ -25,14 +23,6 @@ public class ClassPost {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getClassId() {
-        return classId;
-    }
-
-    public void setClassId(String classId) {
-        this.classId = classId;
     }
 
     public String getAvatar() {
--- app/src/main/java/com/example/pcdashboard/Model/PostComment.java
+++ app/src/main/java/com/example/pcdashboard/Model/PostComment.java
@@ -1,4 +1,47 @@
 package com.example.pcdashboard.Model;
 
 public class PostComment {
+    private String id;
+    private String postId;
+    private String userId;
+    private String content;
+
+    public PostComment(String id, String postId, String userId, String content) {
+        this.id = id;
+        this.postId = postId;
+        this.userId = userId;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
