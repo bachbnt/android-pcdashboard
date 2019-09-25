7b2a5a8d25ee33598d2b9a9a0696c265580b1493
tonbach18598 <51219305+tonbach18598@users.noreply.github.com>
2019-09-25 16:02:30 +0700

    api

---
+++ app/src/main/java/com/example/pcdashboard/API/IAccountService.java
@@ -0,0 +1,35 @@
+package com.example.pcdashboard.API;
+
+import com.example.pcdashboard.Model.BooleanResponse;
+import com.example.pcdashboard.Model.Token;
+import com.example.pcdashboard.Model.User;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface IAccountService {
+    @GET("auth/signin")
+    Call<Token> getToken();
+
+    @FormUrlEncoded
+    @PUT("user/forget-password/{id}")
+    Call<BooleanResponse> forgetPassword(@Path("id") String id);
+
+    @FormUrlEncoded
+    @PUT("user/change-password/{id}")
+    Call<BooleanResponse> changePassword(@Path("id") String id, @Field("oldPassword") String oldPassword, @Field("newPassword") String newPassword);
+
+    @FormUrlEncoded
+    @PUT("user/update-info/{id}")
+    Call<BooleanResponse> updateInfo(@Path("id") String id, @Field("email") String email, @Field("phone") String phone);
+
+    //User
+    @GET("user/{localId}")
+    Call<ArrayList<User>> getAllUsers(@Path("localId") String localId);
+}
