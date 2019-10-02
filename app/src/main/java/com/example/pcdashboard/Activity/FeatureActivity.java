9aa88086c8eb6ea30caf64b70b5d38a50e09910a
tonbach18598 <51219305+tonbach18598@users.noreply.github.com>
2019-10-02 21:32:28 +0700

    info,password,post UI

--- app/src/main/java/com/example/pcdashboard/Activity/FeatureActivity.java
+++ app/src/main/java/com/example/pcdashboard/Activity/FeatureActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.pcdashboard.Fragment.ClassFragment;
 import com.example.pcdashboard.Fragment.InfoFragment;
 import com.example.pcdashboard.Fragment.PasswordFragment;
+import com.example.pcdashboard.Fragment.PostFragment;
 import com.example.pcdashboard.R;
 
 public class FeatureActivity extends AppCompatActivity {
@@ -14,6 +16,6 @@ public class FeatureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container_feature,new InfoFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_feature,new PostFragment()).commit();
     }
 }
